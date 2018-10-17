bca5e9e84b39053dbcec26755927c6b709f9af25
winter2012 <winter2012102@gmail.com>
2018-10-17 10:22:47 +0800

    idea

--- test.php
+++ test.php
@@ -161,4 +161,5 @@
 //echo substr(strstr($result['htmlText'],"href="),6,strpos(strstr($result['htmlText'],"href="), ';}')-7);
 //echo strpos(strstr($result['htmlText'],"href="), ';}');
 
-echo time();
+date_default_timezone_set('PRC');
+echo date("Y-m-d H:i:s",strtotime("-30 minutes"));
