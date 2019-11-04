d6bce6b729849a22a00dc00e35cf2adba8df7d34
winter2012 <winter2012102@gmail.com>
2019-11-04 20:46:21 +0800

    波比数据处理

---
+++ bobi/insert.php
@@ -0,0 +1,28 @@
+<?php
+$file = file_get_contents('JsonToSql.txt');
+$strArray = explode("\n",$file);
+$data = [];
+foreach ($strArray as $item){
+    $pickData = json_decode($item,320);
+    $tmpData = rtrim($pickData['Data'],'}');
+    $tmpData = ltrim($tmpData,'{');
+    $primaryData = explode('},{',$tmpData);
+    foreach ($primaryData as $datum){
+        $datum = "{".$datum."}";
+        $data[] = json_decode($datum,320);
+    }
+}
+$stream = fopen("table.txt", "w+");
+//foreach ($data[0] as $key=>$value){
+//    fwrite($stream,$key."/");
+//}
+//fwrite($stream,"\r\n");
+foreach ($data as $datum){
+    if( isset($datum['RegisterMobile']) && $datum['RegisterMobile'] != null ){
+//        foreach ($datum as $key=>$value){
+//            fwrite($stream,$value."/");
+//        }
+        fwrite($stream,$datum['RegisterMobile']);
+        fwrite($stream,"\r\n");
+    }
+}
